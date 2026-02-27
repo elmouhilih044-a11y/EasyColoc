49ea0e0ad36188c7128d0d870f4883899181ba7b
Hajar <elmouhili.h044@ucd.ac.ma>
2026-02-27 11:28:33 +0000

    feat:add fonction leave controller membership

--- app/Http/Controllers/MembershipController.php
+++ app/Http/Controllers/MembershipController.php
@@ -10,56 +10,15 @@ class MembershipController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
+
+
+      public function leave(Membership $membership)
     {
-        //
+        $membership->update(['left_at' => now()]);
+        return redirect()->back()->with('success', 'Vous avez quitté la colocation.');
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(Membership $membership)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Membership $membership)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, Membership $membership)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Membership $membership)
-    {
-        //
-    }
+   
 }
